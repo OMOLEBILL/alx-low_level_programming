a1d096c7b8cfe1f45119895b1847ce74a1a5c6ff
OMOLEBILL <105784385+OMOLEBILL@users.noreply.github.com>
2022-06-30 23:41:34 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 
 	printf("Size of a char : %lu byte(s)\n", sizeof(a));
 	printf("Size of an int : %lu byte(s)\n", sizeof(b));
-	printf("Size of a long int : %lu byte(s)\n", sizeof(c));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
 	printf("Size of a long long : %lu byte(s)\n", sizeof(d));
 	printf("Size of a float : %lu byte(s)\n", sizeof(e));
 
