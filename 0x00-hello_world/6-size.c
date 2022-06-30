3d027511a428fde71e34083752346da039afffb2
OMOLEBILL <omolebill01@gmail.com>
2022-06-30 14:51:36 -0700

    low level language

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,15 +9,15 @@ int main(void)
 {
 	char      a;
 	int       b;
-	long int  c;
+	long      c;
 	long long d;
 	float     e;
 
-	printf("Size of a char : %lu byte(s)\n", sizeof(a));
-	printf("Size of an int : %lu byte(s)\n", sizeof(b));
+	printf("Size of a char: %lu byte(s)\n", sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
-	printf("Size of a long long : %lu byte(s)\n", sizeof(d));
-	printf("Size of a float : %lu byte(s)\n", sizeof(e));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", sizeof(e));
 
 	return (0);
 }
