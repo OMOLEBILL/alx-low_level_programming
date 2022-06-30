0328f4a0e843db3d4381595a2c7aa2d24a483e1d
OMOLEBILL <omolebill01@gmail.com>
2022-06-30 14:21:42 -0700

    low level language

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (good)
+ */
+int main(void)
+{
+	char      a;
+	int       b;
+	long      c;
+	long long d;
+	float     e;
+
+	printf("Size of a char : %lu byte(s)\n", sizeof(a));
+	printf("Size of an int : %lu byte(s)\n", sizeof(b));
+	printf("Size of a long : %lu byte(s)\n", sizeof(c));
+	printf("Size of a long long : %lu byte(s)\n", sizeof(d));
+	printf("Size of a float : %lu byte(s)\n", sizeof(e));
+
+	return (0);
+}
