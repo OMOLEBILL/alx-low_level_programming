1e53ee4810e0212ee17a202eb9666fc9f1d6cda6
OMOLEBILL <omolebill01@gmail.com>
2022-07-01 13:33:58 -0700

    conditional statements

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints all possible combinations of single-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		putchar(c + '0');
+		if (c < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
