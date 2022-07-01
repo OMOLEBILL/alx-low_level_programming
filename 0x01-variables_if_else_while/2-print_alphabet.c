199bc08967bd9caf44e67222ff71dcc66f29caee
OMOLEBILL <omolebill01@gmail.com>
2022-07-01 10:45:54 -0700

    conditional statements

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Description: print A-Z
+ *
+ * Return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+		putchar(c);
+	return (0);
+}
