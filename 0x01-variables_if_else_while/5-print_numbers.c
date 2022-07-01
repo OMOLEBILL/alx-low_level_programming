ec819f7af242eca6ac32a966e6b2768f668fcbf3
OMOLEBILL <omolebill01@gmail.com>
2022-07-01 12:07:52 -0700

    conditional statements

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Integers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; ++a)
+		printf("%i", a);
+	putchar('\n');
+
+	return (0);
+}
