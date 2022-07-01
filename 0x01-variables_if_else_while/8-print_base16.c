e3fb81502672e87fb04a95805e59615752d696e9
OMOLEBILL <omolebill01@gmail.com>
2022-07-01 12:53:24 -0700

    conditional statements

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include < stdio.h>
+
+/**
+ * Main - Hexadecimals numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 0; c <= 9; c++)
+		putchar(c);
+	for(c = 'a'; c <= 'f'; c++)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
