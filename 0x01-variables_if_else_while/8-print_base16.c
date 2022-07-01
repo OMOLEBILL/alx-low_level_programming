f530410038f9bc100a4ac03538cce41f7ff25489
OMOLEBILL <105784385+OMOLEBILL@users.noreply.github.com>
2022-07-01 21:59:07 +0200

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
 /**
- * Main - Hexadecimals numbers
- *
- * Return: Always 0 (Success)
- */
-
+* main - Print the numbers of base 16 in lowercase
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	char c;
+		char c;
 
-	for (c = 0; c <= 9; c++)
+		for (c = '0'; c <= '9'; c++)
 		putchar(c);
-	for(c = 'a'; c <= 'f'; c++)
+
+		for (c = 'a'; c <= 'f'; c++)
 		putchar(c);
-	putchar('\n');
+
+		putchar('\n');
 
 	return (0);
 }
