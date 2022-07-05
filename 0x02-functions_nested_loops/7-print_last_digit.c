6d78a5db2b3580201701d0d9e4173b3f5a70346e
OMOLEBILL <omolebill01@gmail.com>
2022-07-05 13:03:59 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_last_digit - this fucntion prints the last digit of no
+ *
+ * @n: ASCII Character
+ *
+ * Return: the last digit.
+ */
+int print_last_digit(int n)
+{
+	int m;
+
+	m = n % 10;
+	if (m < 0)
+	{
+		_putchar(-m + 48);
+		return (-m);
+	}
+	else 
+	{
+		_putchar(m + 48);
+		return (m);
+	}
+}
+
