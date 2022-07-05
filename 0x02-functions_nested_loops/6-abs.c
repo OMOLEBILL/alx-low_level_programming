70edca634fdb9960f1459fb6bee5a7d0503613ad
OMOLEBILL <omolebill01@gmail.com>
2022-07-05 12:38:06 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * int _abs - give the absolute value of a number
+ *
+ * @n: ASCII character
+ *
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
