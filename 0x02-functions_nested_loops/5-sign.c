774975b0683476f67ef640b873bcfddda8a3bc56
OMOLEBILL <omolebill01@gmail.com>
2022-07-05 12:16:23 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - prints + if >  0 , prints - if < 0
+ *
+ * @n: ASCII Character
+ *
+ * Return: 1, 0 or -1 if positive/ negative
+ */
+int print_sign(int n)
+{
+	if (n > 1)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n <1)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else 
+	{
+		_putchar(48);
+		return (0);
+	}
+	_putchar('\n')
+}
