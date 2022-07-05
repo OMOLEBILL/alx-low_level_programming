6bd7d1ba2d91414e27e8f950f4f8acd8d9060df2
OMOLEBILL <105784385+OMOLEBILL@users.noreply.github.com>
2022-07-05 22:49:42 +0200

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,2 +1,33 @@
 #include "main.h"
 
+/**
+* times_table - prints the 9 times table
+*
+* Return: void
+*/
+void times_table(void)
+{
+	int m, n, o;
+
+	for (m = 0; m <= 9; m++)
+	{
+		_putchar(48);
+		for (n = 1; n <= 9; n++)
+		{
+			o = m * n;
+			_putchar(44);
+			_putchar(32);
+			if (o <= 9)
+			{
+				_putchar(32);
+				_putchar(o + 48);
+			}
+			else
+			{
+				_putchar((o / 10) + 48);
+				_putchar((o % 10) + 48);
+			}
+		}
+	_putchar('\n');
+	}
+}
