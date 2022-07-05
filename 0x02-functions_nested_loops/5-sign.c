8b7785d8732224e9a4d658e4e8f2aea9f5ecbd72
OMOLEBILL <105784385+OMOLEBILL@users.noreply.github.com>
2022-07-05 21:23:13 +0200

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,20 +9,20 @@
  */
 int print_sign(int n)
 {
-	if (n > 1)
-	{
+	if (n > 0)
+		{
 		_putchar(43);
-		return (1);
-	}
-	else if (n <1)
-	{
+	return (1);
+		}
+	else if (n < 0)
+		{
 		_putchar(45);
-		return (-1);
-	}
-	else 
-	{
+	return (-1);
+		}
+	else
+		{
 		_putchar(48);
-		return (0);
-	}
-	_putchar('\n');
+	return (0);
+		}
+		_putchar('\n');
 }
