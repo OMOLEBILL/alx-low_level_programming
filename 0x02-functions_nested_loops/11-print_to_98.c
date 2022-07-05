a8c1e624b59b7ac817c8196e99d5b6a9a9ad8856
OMOLEBILL <105784385+OMOLEBILL@users.noreply.github.com>
2022-07-05 23:10:58 +0200

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,2 +1,28 @@
 #include "main.h"
 
+/**
+* print_to_98 - prints all natural numbers
+* from n to 98, followed by a new line
+*
+* @n: input number.
+*
+* Return: void.
+*/
+void print_to_98(int m)
+{
+	if (m > 98)
+	{
+	for (; m > 98; m--)
+		{
+		printf("%d, ", n);
+		}
+	}
+	else if (m < 98)
+	{
+		for (; m < 98; m++)
+		{
+		printf("%d, ", m);
+		}
+	}
+	printf("%d\n", m);
+}
