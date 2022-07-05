716cf3846960f91d6ad1cee51b71555f718ae138
OMOLEBILL <105784385+OMOLEBILL@users.noreply.github.com>
2022-07-05 23:23:35 +0200

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,9 +13,9 @@ void print_times_table(int n)
 	{
 	for (a = 0; a <= n; a++)
 	{
-	_putchar(48);
-	for (b = 1; b <= n; b++)
-	{
+	 _putchar(48);
+	 for (b = 1; b <= n; b++)
+	 {
 		op = a * b;
 		_putchar(44);
 		_putchar(32);
@@ -37,7 +37,7 @@ void print_times_table(int n)
 		_putchar(((op / 10) % 10) + 48);
 		_putchar((op % 10) + 48);
 		}
-	}
+	 }
 	_putchar('\n');
 	}
 	}
