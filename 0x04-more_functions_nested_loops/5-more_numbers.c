5c9477d3d3bb90027a72907d85c258b52728c4c2
OMOLEBILL <105784385+OMOLEBILL@users.noreply.github.com>
2022-07-07 16:41:39 +0200

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,17 +8,14 @@ void more_numbers(void)
 	int a = 0;
 	char c;
 
-	while (a <10)
+	for (a = 0; a < 10; a++)
 	{
 		for (c = 0; c <= 14; c++)
 		{
 			if (c > 9)
-			{
-				_putchar((c / 10) + '0');
-				_putchar((c / 10) + '0');
-			}
+			_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
 		}
 		_putchar('\n');
-		a++;
 	}
 }
