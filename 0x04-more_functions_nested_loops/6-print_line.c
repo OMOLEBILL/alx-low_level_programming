dc34f436e5b25637fc8733b68b954e5df0b15663
OMOLEBILL <omolebill01@gmail.com>
2022-07-07 07:50:06 -0700

    more fuctions

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - print line according to user input
+ * @n: user input
+ * Return: void
+ */
+void print_line(int n)
+{
+	char a;
+
+	for (a = 0; a < n; a++)
+		_putchar('_');
+	_putchar('\n');
+}
+	
