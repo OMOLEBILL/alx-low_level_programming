19e5911e22e19b4367025a2dd1e4ea4c2a06a7f9
OMOLEBILL <omolebill01@gmail.com>
2022-07-07 07:17:10 -0700

    more fuctions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints most numbers except 4&2
+ */
+void print_most_numbers(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		if (a != '2' && a != '4')
+		       _putchar(a);
+	}
+        _putchar('\n');
+}	
+
