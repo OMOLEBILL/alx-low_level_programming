f8b81ec6b2403abe7fd935f31c12ff04e7bf1511
OMOLEBILL <105784385+OMOLEBILL@users.noreply.github.com>
2022-07-07 16:55:44 +0200

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,15 +1,13 @@
 #include "main.h"
-
 /**
 * print_line - Write a function that draws a straight line in the terminal.
 * @n: Integer amount of _
 */
 void print_line(int n)
 {
-	char a;
+	char c;
 
-	for (a = 0; a < n; a++)
+	for (c = 0; c < n; c++)
 		_putchar('_');
 	_putchar('\n');
 }
-	
