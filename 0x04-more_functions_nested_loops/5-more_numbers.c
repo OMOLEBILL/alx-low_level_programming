e2d6bc2a46932b4168eac00ffb9e7ab6a372ddde
OMOLEBILL <omolebill01@gmail.com>
2022-07-07 07:38:40 -0700

    more fuctions

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - Print numbers from 1- 14 10 times
+ */
+void more_numbers(void)
+{
+	int a = 0;
+	char c;
+
+	while (a <10)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c > 9)
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c / 10) + '0');
+			}
+		}
+		_putchar('\n');
+		a++;
+	}
+}
