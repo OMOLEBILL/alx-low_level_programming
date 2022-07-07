82c32bfa2bf26aba82035866b4e94edffcc5ca1d
OMOLEBILL <omolebill01@gmail.com>
2022-07-07 00:28:35 -0700

    more fuctions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - mproduct of two integers
+ * @a: ASCII character
+ * @b: ASCII character
+ * Return: Product of two numbers
+ */
+int mul(int a, int b)
+{
+	int z;
+
+	z = (a * b);
+
+	return (z);
+}
