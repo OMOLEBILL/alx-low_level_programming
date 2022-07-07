9c18d1aacf30461fc87f84fcd5c417ae03385681
OMOLEBILL <omolebill01@gmail.com>
2022-07-07 08:52:43 -0700

    more fuctions

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints fizz/ buzz or fizzbuzz depending
+ *
+ * Return: Always 0 sucess
+ */
+int main(void)
+{
+	 int a;
+	  
+	 for (a = 1; a <= 100; a++)
+	 {
+		 if (a % 3 == 0)
+		 {
+			 printf("%s ", "Fizz");
+		 }
+		 else if (a % 5 == 0)
+			 printf("%s ",  "Buzz");
+		 else if (a % 3 == 0 && a % 5 == 0)
+			 printf("%s ", "FizzBuzz");
+		 else
+			 printf("%d ",  a);
+	 }
+}
+			 
