1b1a4e747cd4398e83143d181ba294cda7682f1a
OMOLEBILL <omolebill01@gmail.com>
2022-07-11 10:07:19 -0700

    arrays and pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints srings in reverse
+ * @s: the string to be printed
+ */
+void print_rev(char *s);
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+	}
+	for (a = a - 1; a >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n');
+}
+
+
