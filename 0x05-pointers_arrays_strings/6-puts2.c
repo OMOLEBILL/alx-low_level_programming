4bb89392a91d4b1a2d34451a72930686e97f1d2d
OMOLEBILL <omolebill01@gmail.com>
2022-07-11 10:23:16 -0700

    arrays and pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: the string to e printed
+ */
+void puts2(char *str)
+{
+	int len = 0;
+	int l = 0;
+	char *y = str;
+	int z;
+
+	while (*y != '\0')
+	{
+		y++;
+		len++;
+	}
+	l = len - 1;
+	for (z = 0 ; z <= l ; z++)
+	{
+		if (z % 2 == 0)
+		{
+			_putchar(str[z]);
+		}
+	}
+	_putchar('\n');
+}
