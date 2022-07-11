f0f1d85c94280e82cc6b4d028a8ced2318da12e1
OMOLEBILL <omolebill01@gmail.com>
2022-07-11 10:29:18 -0700

    arrays and pointers

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - half of a string then newline
+ * @str: sting to be printed
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	char *y = str;
+	int n;
+
+	while (*y != '\0')
+	{
+		y++;
+		len++;
+	}
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+	}
+	else
+	{
+		n = (len + 1) / 2;
+	}
+	for ( ; n < len ; n++)
+	{
+		_putchar(str[n]);
+	}
+	_putchar('\n');
+}
