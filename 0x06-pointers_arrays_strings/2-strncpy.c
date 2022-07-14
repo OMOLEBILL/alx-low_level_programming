6b98471fa8129b8233b0f56bc2cd6c4273200ad6
OMOLEBILL <omolebill01@gmail.com>
2022-07-14 10:34:11 -0700

    more functions

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: input string
+ * @src: inpput string
+ * @n: number of time to be copied
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+
+	while (a < n && src[a] != '\0')
+	{
+		dest[a] = src[a];
+		a++;
+	}
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
+
+	return (dest)
+}	
