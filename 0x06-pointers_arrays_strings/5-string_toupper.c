c0016ac20c852e3208468c37348841aa08116b7d
OMOLEBILL <105784385+OMOLEBILL@users.noreply.github.com>
2022-07-14 20:09:17 +0200

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,23 +1,21 @@
 #include "main.h"
 
 /**
- * string_toupper - changes lowercse 2 upper
- * @b: pointer
+ * string_toupper - change all lowercase to uppercase
+ * @n: pointer
  *
- * Return: b
+ * Return: n
  */
-char *string_toupper(char *b)
+char *string_toupper(char *n)
 {
-	int a;
+	int i;
 
-	a = 0;
-	while (b[a] != '\0')
+	i = 0;
+	while (n[i] != '\0')
 	{
-		if (b[a] >= 'a' && b[a] <= 'z')
-		{
-			b[a] = b[a] - 32;
-		}
-		a++;
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
 	}
-	return (0);
+	return (n);
 }
