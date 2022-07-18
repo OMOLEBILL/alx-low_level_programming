57611230bd209b6c2dc8e0d1e3cbfe7594412bde
OMOLEBILL <105784385+OMOLEBILL@users.noreply.github.com>
2022-07-18 20:52:23 +0200

    update

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _memset - afuction that prints a value
- * @s: the adrress of the value
- * @b: the value to printed
- * @n the number of bytes to be printed
+ *_memset - afuction that prints a value
+ *@s: the adrress of the value
+ *@b: the value to printed
+ *@n the number of bytes to be printed
  *
- * Return: s the address of the bytes
+ *Return: s the address of the bytes
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int a;
+	int a = 0;
 
 	while (n > 0)
 	{
