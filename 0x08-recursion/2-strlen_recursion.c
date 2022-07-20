4e975c12a89859ddb8d110a31a668f01b17b62c3
OMOLEBILL <omolebill01@gmail.com>
2022-07-20 12:33:24 -0700

    recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - prints length of string
+ * @s: pointer to the string
+ * Return: the length of the string
+ **/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return(0);
+	}
+	return (_strlen_recursion(s + 1) + 1);
+}
