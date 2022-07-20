3a648bfdc0d6c58f5951ab190aabfef8f11560ba
OMOLEBILL <omolebill01@gmail.com>
2022-07-20 12:43:39 -0700

    recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * factorial - returns a factorial
+ * @n: the factorial 
+ * Return: the factorial
+ **/
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
