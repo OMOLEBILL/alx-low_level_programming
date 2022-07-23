b44e6e5363745ad2d02e65bf724bc94567a919f4
OMOLEBILL <105784385+OMOLEBILL@users.noreply.github.com>
2022-07-23 19:20:40 +0200

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,8 +5,8 @@
  * @argv: argument vector
  * Return: always 0 
  **/
-int main (int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d", argc - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
