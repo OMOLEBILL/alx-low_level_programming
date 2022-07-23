3acb046b9c04b2ec8554d22d2f6fc6a6c23eedc4
OMOLEBILL <omolebill01@gmail.com>
2022-07-23 10:45:57 -0700

    arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints multipication
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always 0 sucess
+ **/
+int main (int argc; char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
