dc6c027ed41a94f130b6a7352483b2b8036cd378
OMOLEBILL <omolebill01@gmail.com>
2022-07-24 02:54:28 -0700

    arguments

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the change of a given amount
+ * @argc: argument count
+ * @argv: argument array
+ * Return: always return 0 sucessss
+ **/
+int main (int argc, char *argv[])
+{
+	int cents, number = 0;
+
+	if (argc == 1 || argc > 2)
+	{
+		printf("Error\n");
+		return(0);
+	}
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0 )
+	{
+		if (cents >= 25)
+			cents -= 25;
+		else if  (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		number += 1;
+	}
+	printf("%d\n", number);
+	return (0);
