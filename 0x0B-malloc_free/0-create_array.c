635ccaac83f8cf10de3d38bdba3bbf6bb5aac640
OMOLEBILL <omolebill01@gmail.com>
2022-07-25 09:57:54 -0700

    malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * create_array - creates an array of characters
+ * @size: size of the array
+ * @c: the charcter making the array
+ * Return: a apointerto the array
+ **/
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	p = malloc(sizeof(c) * size);
+	if (p == 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		p[i] = c;
+	return (p);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+ I try to allocate memory using malloc and free using free
