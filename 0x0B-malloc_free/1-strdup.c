cb3579977efdbfc8d25c0663bbdb043773165715
OMOLEBILL <omolebill01@gmail.com>
2022-07-25 10:55:01 -0700

    malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - return a pointer to a string
+ * @str: the string to be used
+ * Return: pointer to the sring
+ **/
+char *_strdup(char *str)
+{
+	char string;
+	unsigned int i, j;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != 0; i++)
+		;
+	string = (char *)malloc(sizeof(char) * (i + 1));
+
+	if (string == NULL)
+		return (NULL);
+	for (j = 0; j <= i; j++)
+		string[j] = str[j];
+	return (string);
+}
