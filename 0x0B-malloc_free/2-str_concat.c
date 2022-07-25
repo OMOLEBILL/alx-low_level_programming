44d55ce77ecf3e7e400c0d15d8ec7da8928f3c7e
OMOLEBILL <105784385+OMOLEBILL@users.noreply.github.com>
2022-07-25 20:36:39 +0200

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,29 +8,34 @@
  **/
 char *str_concat(char *s1, char *s2)
 {
-	char *str;
-	unsigned int i, j, k, v;
+	char *strout;
+	unsigned int i, j, k, limit;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (i = 0; i != '\0'; i++)
+
+	for (i = 0; s1[i] != '\0'; i++)
 		;
-	for (j = 0; j != '\0'; j++)
+
+	for (j = 0; s2[j] != '\0'; j++)
 		;
-	str = malloc(sizeof(char) * (i + j + 1));
-	if (str == NULL)
+
+	strout = malloc(sizeof(char) * (i + j + 1));
+
+	if (strout == NULL)
 	{
-		free(str);
+		free(strout);
 		return (NULL);
 	}
 
 	for (k = 0; k < i; k++)
-		str[k] = s1[k];
+		strout[k] = s1[k];
 
-	v = j;
-	for (j = 0; j <= v; k++, j++)
-		str[k] = s2[j];
-	return (str);
+	limit = j;
+	for (j = 0; j <= limit; k++, j++)
+		strout[k] = s2[j];
+
+	return (strout);
 }
