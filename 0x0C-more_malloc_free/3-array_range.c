1808c350169925a4d52687666806673f9c954161
OMOLEBILL <omolebill01@gmail.com>
2022-07-28 13:08:28 -0700

    more mallocs

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ **array_range - arranges the arrasy
+ * @min: the minimum number given
+ * @max: the max number given
+ * Return: the arrays in order
+ **/
+int *array_range(int min, int max)
+{
+	int *j;
+	int i, no;
+
+	if (min > max)
+		return (NULL);
+	no = (max - min) + 1;
+	j = malloc(size * sizeof(*j));
+	if (j == NULL)
+		return (NULL);
+	for (i = 0; i < no && min <= max; i++, min++)
+		*(j + i) = min;
+	return (j);
+}
