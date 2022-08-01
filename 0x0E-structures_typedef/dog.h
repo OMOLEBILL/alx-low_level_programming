c324b9263160b72c8bfb0bbb1c0dc610e9f92676
OMOLEBILL <omolebill01@gmail.com>
2022-08-01 11:00:11 -0700

    preprocessors

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Structure of dog
+ * @name: name of dog
+ * @age: dog age
+ * @owner: dog's owner
+ * Description: Structure of a dog
+ **/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
