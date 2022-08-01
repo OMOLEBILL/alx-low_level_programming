1e04a60539109d1c1e68e54d8a4de7319f1af117
OMOLEBILL <omolebill01@gmail.com>
2022-08-01 11:07:56 -0700

    preprocessors

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - initializa a dog stuct
+ * @d: pointer
+ * @name: pointer to name 
+ * @age: pointer to age
+ * @owner: owner of the dog
+ **/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
