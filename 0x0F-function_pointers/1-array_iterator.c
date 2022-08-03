899eea875d55a01c0d95321dbe189ad995fdab73
OMOLEBILL <omolebill01@gmail.com>
2022-08-03 11:06:21 -0700

    fpoints

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * array_iterator - iteriates through eery element of an array
+ * @array: pointer to the array
+ * @size: the size of the array
+ * @action: the function 
+ **/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (!array || !action)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
