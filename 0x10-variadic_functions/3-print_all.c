47a0b570f9150f389ea30072bf70efcbc8347316
OMOLEBILL <omolebill01@gmail.com>
2022-08-04 13:25:17 -0700

    riadic fun

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,18 +2,17 @@
 
 /**
  * print_all - prints anything.
- * @format: a list of types of arguments passed to the function.
- *
- * Return: no return.
- */
+ * @format: a list of types 
+ * Return: void
+ **/
 void print_all(const char * const format, ...)
 {
-	va_list valist;
+	va_list args;
 	unsigned int i = 0, j, c = 0;
 	char *str;
 	const char t_arg[] = "cifs";
 
-	va_start(valist, format);
+	va_start(args, format);
 	while (format && format[i])
 	{
 		j = 0;
@@ -28,16 +27,16 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 		case 'c':
-			printf("%c", va_arg(valist, int)), c = 1;
+			printf("%c", va_arg(args, int)), c = 1;
 			break;
 		case 'i':
-			printf("%d", va_arg(valist, int)), c = 1;
+			printf("%d", va_arg(args, int)), c = 1;
 			break;
 		case 'f':
-			printf("%f", va_arg(valist, double)), c = 1;
+			printf("%f", va_arg(args, double)), c = 1;
 			break;
 		case 's':
-			str = va_arg(valist, char *), c = 1;
+			str = va_arg(args, char *), c = 1;
 			if (!str)
 			{
 				printf("(nil)");
@@ -47,5 +46,5 @@ void print_all(const char * const format, ...)
 			break;
 		} i++;
 	}
-	printf("\n"), va_end(valist);
+	printf("\n"), va_end(args);
 }
