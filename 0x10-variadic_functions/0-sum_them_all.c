f9f51ca343ec9caf964e9cefb975fdf94a72cce1
OMOLEBILL <omolebill01@gmail.com>
2022-08-04 12:35:54 -0700

    riadic fun

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,6 +1,4 @@
 #include "variadic_functions.h"
-#include <stdio.h>
-#include <stdarg.h>
 /**
  * sum_them_all - sums the number of arguments
  * @n: number of arguements
@@ -8,17 +6,19 @@
  **/
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0, i;
+	int sum = 0;
+	unsigned int i;
+        va_list n;
 
 	if (n == 0)
 		return (0);
-        va_list args;
-
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 		sum += va_arg(args, int);
-
 	va_end(args);
 	return (sum);
 }
+
+	
+
