6d3a2e77308c6fed96bff29ee194fc3a58ce8054
OMOLEBILL <105784385+OMOLEBILL@users.noreply.github.com>
2022-08-11 22:55:48 +0200

    linking

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,9 +7,9 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	size_t elem;
 
-	nelem = 0;
+	elem = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		nelem++;
+		elem++;
 	}
-	return (nelem);
+	return (elem);
 }
