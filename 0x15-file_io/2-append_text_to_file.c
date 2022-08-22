09507fe0b73f56efc0290e72489947f0fe602fcc
OMOLEBILL <105784385+OMOLEBILL@users.noreply.github.com>
2022-08-22 22:21:57 +0200

    file ops

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,19 +1,17 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * append_text_to_file - appends text at the end
- * @filename: a pointer to the filename
- * @text_content: a pointer to the string
- * 
- * Return: -1 if function fails or filename is 
- *            null and file doesn't exist
- *            else -1
- **/
-int append_text_to_file(const char *filename,
-	       	char *text_content)
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: A pointer to the name of the file.
+ * @text_content: The string to add to the end of the file.
+ *
+ * Return: If the function fails or filename is NULL - -1.
+ *         If the file does not exist the user lacks write permissions - -1.
+ *         Otherwise - 1.
+ */
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int op, wr, len = 0;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -24,13 +22,13 @@ int append_text_to_file(const char *filename,
 			len++;
 	}
 
-	op = open(filename, O_WRONLY | O_APPEND);
-	wr = write(op, text_content, len);
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
 
-	if (op == -1 || wr == -1)
+	if (o == -1 || w == -1)
 		return (-1);
 
-	close(op);
+	close(o);
 
 	return (1);
 }
