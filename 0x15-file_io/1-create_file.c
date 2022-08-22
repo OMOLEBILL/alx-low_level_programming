eee656ca7ea7acc091f9ce6a03f3fb270ab67fb8
OMOLEBILL <omolebill01@gmail.com>
2022-08-22 12:34:08 -0700

    file ops

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@
  **/
 int create_file(const char *filename, char *text_content)
 {
-	int op, re, len;
+	int op, wr, len;
 
 	if (filename == NULL)
 		return (-1);
@@ -22,9 +22,9 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	re = read(op, text_content, len);
+	wr = write(op, text_content, len);
 
-	if (op == -1 || re == -1 )
+	if (op == -1 || wr == -1 )
 		return (-1);
 
 	close(op);
