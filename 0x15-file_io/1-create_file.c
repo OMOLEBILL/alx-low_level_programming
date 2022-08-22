8a67f46f61e51e74baf9658526e91ba44acf4b4c
OMOLEBILL <105784385+OMOLEBILL@users.noreply.github.com>
2022-08-22 22:18:26 +0200

    file ops

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,16 +1,16 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * create_file - function that creates a file
- * @filename: name of the file 
- * @text_content: sting to write 
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
  * Return: If the function fails - -1.
- *         else -1
- **/
+ *         Otherwise - 1.
+ */
 int create_file(const char *filename, char *text_content)
 {
-	int op, wr, len;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -21,13 +21,13 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wr = write(op, text_content, len);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
 
-	if (op == -1 || wr == -1 )
+	if (o == -1 || w == -1)
 		return (-1);
 
-	close(op);
-	
+	close(o);
+
 	return (1);
 }
