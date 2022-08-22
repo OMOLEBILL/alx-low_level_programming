1647abde21449960f273fd5adaf49778b1aadce3
OMOLEBILL <105784385+OMOLEBILL@users.noreply.github.com>
2022-08-22 22:20:04 +0200

    file ops

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - Reads a text file and prints it.
- * @filename: A pointer to the name of the file
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
  * @letters: The number of letters the
- *           to be read
- * Return: If the function fails or filename is
- *         NULL and the actual bytes
- **/
+ *           function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t o, r, w;
