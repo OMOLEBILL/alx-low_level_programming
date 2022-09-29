a8e7200106e9bb3a4a7b002ee9f13f3e12f68201
OMOLEBILL <105784385+OMOLEBILL@users.noreply.github.com>
2022-09-29 23:28:59 +0200

    doubles

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,23 +1,22 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the elements
+ * dlistint_len - counts and returns number of elements of a dlistint_t list.
  * @h: pointer to head of the list
  * Return: number of nodes in the list
  **/
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t size;
 
 	size = 0;
 	if (h == NULL)
-		return (size);
+	return (size);
 
 	while (h)
 	{
-		size++;
-		h = h->next;
+	size++;
+	h = h->next;
 	}
-
 	return (size);
 }
