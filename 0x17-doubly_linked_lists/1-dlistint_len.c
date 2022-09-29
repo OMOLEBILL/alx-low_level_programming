d39c0d87cd8d822e7a600eafee65d2dfa859ab9e
OMOLEBILL <omolebill01@gmail.com>
2022-09-29 14:12:29 -0700

    double

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements
+ * @h: pointer to head of the list
+ * Return: number of nodes in the list
+ **/
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t size;
+
+	size = 0;
+	if (h == NULL)
+		return (size);
+
+	while (h)
+	{
+		size++;
+		h = h->next;
+	}
+
+	return (size);
+}
