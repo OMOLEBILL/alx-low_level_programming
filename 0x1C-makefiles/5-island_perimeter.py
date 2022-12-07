211507aacd5d0c506c94fa28457740a8d9db0155
OMOLEBILL <omolebill01@gmail.com>
2022-12-07 23:23:34 +0000

    bug

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,8 +8,8 @@ def island_perimeter(grid):
         for j in range(n):
             if grid[i][j] == 1:
                 land += 1
-                if i < m - 1 and grid[i+1][j] == 1:
+                if (i < (m - 1) and grid[i+1][j] == 1):
                     nei += 1
-                if j < n - 1 and grid[i][j+1] == 1:
+                if (j < (n - 1) and grid[i][j+1] == 1):
                     nei += 1
-    return (land*4 - 2*nei)
+    return land*4 - 2*nei
