587466b11fcb89b2bd2a27b69866869e3a7d3ef7
bill omole <omolebill01@gmail.com>
2023-01-29 14:39:44 +0100

    ifndef

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,8 +1,8 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-#ifndef SEARCH_ALGOS
-#define SEARCH_ALGOS
 
 int linear_search(int *array, size_t size, int value);
 
